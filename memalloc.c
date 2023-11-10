bab1a705a80933a96551e2a1b0053b61f4618f4c
Caio H. R. R. <caiohrr@gmail.com>
2023-11-10 09:22:31 -0300

    [Fix] Código em C funcionando

--- memalloc.c
+++ memalloc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include "memalloc.h"
 
 typedef struct MemoryRegister {
@@ -37,42 +38,35 @@ void *memory_alloc(unsigned long int bytes) {
                 void *tmp_brk = original_brk;
                 MemoryRegister current_mem_reg;
 
-                //current_mem_reg = *(MemoryRegister *) tmp_brk;
-
-                //printMemReg(current_mem_reg);
-                //printf("curr_brk = %p\n", current_brk);
-                //printf("brk = %p\n", tmp_brk);
-                //printf("brk+ = %p\n", tmp_brk + sizeof(MemoryRegister) + current_mem_reg.bytes);
-                //return NULL;
-
                 while ((unsigned long int) tmp_brk < (unsigned long int) current_brk) {
                         current_mem_reg = *(MemoryRegister *) tmp_brk;
 
-                        printMemReg(current_mem_reg);
-                        printf("brk = %p\n", tmp_brk);
                         // Bloco atual está em uso, se for o caso pula para o próximo
                         if (current_mem_reg.use == 1) {
-                                //printf("YES\n");
                                 tmp_brk += sizeof(MemoryRegister) + current_mem_reg.bytes;
                         } else {
-                                // Bloco não está em uso e comporta a alocação atual
+                                // Bloco não está em uso e comporta a nova alocação
                                 if (current_mem_reg.bytes > bytes) {
 
-                                        printf("inside if\n");
+                                        // Escreve o novo registro de alocação em cima do anterior
                                         mem_reg.use = 1;
                                         mem_reg.bytes = bytes;
                                         *(MemoryRegister *) tmp_brk = mem_reg;
                                         void *return_brk = (tmp_brk + sizeof(MemoryRegister));
 
+                                        // Escreve o registro que "sobrou"
                                         mem_reg.use = 0;
                                         mem_reg.bytes = current_mem_reg.bytes - (bytes + sizeof(MemoryRegister));
                                         *(MemoryRegister *) ((unsigned long int) tmp_brk + bytes + sizeof(MemoryRegister)) = mem_reg;
 
                                         return return_brk;
                                 }
+
+                                // Bloco não está em uso e não comporta a nova alocação
                                 tmp_brk += sizeof(MemoryRegister) + current_mem_reg.bytes;
                         }
                 }
+
                 // Não foi encontrado um bloco disponível, incrementa o valor de brk;
                 sbrk(bytes + sizeof(MemoryRegister));
                 mem_reg.use = 1;
@@ -88,56 +82,69 @@ int memory_free(void *pointer) {
 
         void *current_brk = sbrk(0);
         // Se o ponteiro aponta para um valor acima de brk retorna erro
+        // Não sei se é necessário testar
         if ((unsigned long int) pointer > (unsigned long int) current_brk)
                 return -1;
 
-        //unsigned long int *ptr = (unsigned long int *) pointer;
-        printf("\npointer = %p\n", pointer);
-        printf("val = %ld | pointer - 2 = %p\n", *(unsigned long int *) (pointer - 16), pointer - 16);
-        printf("val = %ld | pointer - 1 = %p\n", *(unsigned long int *) (pointer - 8), pointer - 8);
-
+        // Escreve 0 em cima do valor que indica se o bloco está sendo usado
         *(unsigned long int *) (pointer - sizeof(MemoryRegister)) = 0;
 
         return 0;
 }
 
-int main() {
+void double_alloc_after_free(void **pointers_array){
+	pointers_array[1] = memory_alloc(50);
+	pointers_array[2] = memory_alloc(100);
+}
 
-        setup_brk();
-        
-        void *ptr2 = memory_alloc(1024);
-        void *ptr = memory_alloc(100);
+void simple_alloc(void **pointers_array){
+	pointers_array[0] = memory_alloc(100);
+}
 
-        printf("%p, %p\n", ptr, ptr2);
-        
-        char *string = (char *) ptr;
-        int *int_arr = (int *) ptr2;
+int main() { 
 
-        string[0] = 'P';
-        string[1] = 'o';
-        string[2] = 'g';
-        string[3] = 'g';
-        string[4] = 'e';
-        string[5] = 'r';
-        string[6] = 's';
-        string[7] = '\0';
+	printf("============================== ROTINAS DE TESTE ==============================\n");
+	
+	setup_brk();
+	void *initial_brk = original_brk;
+	void *alloc_pointers[3];
 
-        for (int i = 0; i < 1024 / sizeof(int); i ++) {
-                int_arr[i] = i + 1;
-                printf("%d, ", int_arr[i]);
-        }
+	simple_alloc(alloc_pointers);
+	printf("==>> ALOCANDO UM ESPAÇO DE 100 BYTES:\n");
+	printf("\tLOCAL: %s\n", alloc_pointers[0]-16 == initial_brk ? "CORRETO!" : "INCORRETO!");
+	printf("\tIND. DE USO: %s\n", *((long long*) (alloc_pointers[0]-16)) == 1 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (alloc_pointers[0]-8)) == 100 ? "CORRETO!" : "INCORRETO!");
 
-        printf("\n%s\n", string);
+	printf("==>> DESALOCANDO UM ESPAÇO DE 100 BYTES:\n");
+	memory_free(alloc_pointers[0]);
+	printf("\tIND. DE USO: %s\n", *((long long*) (alloc_pointers[0]-16)) == 0 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (alloc_pointers[0]-8)) == 100 ? "CORRETO!" : "INCORRETO!");
 
-        if (memory_free(ptr) == 0)
-                printf("liberou com sucesso\n");
+	double_alloc_after_free(alloc_pointers);
+	printf("==>> ALOCANDO UM ESPAÇO DE 50 BYTES:\n");
+	printf("\tLOCAL: %s\n", alloc_pointers[1]-16 == initial_brk ? "CORRETO!" : "INCORRETO!");
+	printf("\tIND. DE USO: %s\n", *((long long*) (alloc_pointers[1]-16)) == 1 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (alloc_pointers[1]-8)) == 50 ? "CORRETO!" : "INCORRETO!");
+	printf("==>> ALOCANDO UM ESPAÇO DE 100 BYTES:\n");
+	printf("\tLOCAL: %s\n", alloc_pointers[1]+100 == alloc_pointers[2]-16 ? "CORRETO!" : "INCORRETO!");
+	printf("\tIND. DE USO: %s\n", *((long long*) (alloc_pointers[2]-16)) == 1 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (alloc_pointers[2]-8)) == 100 ? "CORRETO!" : "INCORRETO!");
+	printf("==> VERIFICANDO A FRAGMENTAÇÃO DE MEMÓRIA:\n");
+	printf("\tIND. DE USO: %s\n", *((long long*) (alloc_pointers[1]+50)) == 0 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (alloc_pointers[1]+58)) == 34 ? "CORRETO!" : "INCORRETO!");
 
-        void *ptr3 = memory_alloc(50);
-        printf("%p", ptr3);
-        char *string2 = (char *) ptr3;
+	printf("==>> DESALOCANDO TUDO:\n");
+	memory_free(alloc_pointers[1]);
+	memory_free(alloc_pointers[2]);
+	printf("\tIND. DE USO: %s\n", *((long long*) (alloc_pointers[1]-16)) == 0 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (alloc_pointers[1]-8)) == 50 ? "CORRETO!" : "INCORRETO!");
+	printf("\tIND. DE USO: %s\n", *((long long*) (alloc_pointers[2]-16)) == 0 ? "CORRETO!" : "INCORRETO!");
+	printf("\tTAMANHO: %s\n", *((long long*) (alloc_pointers[2]-8)) == 100 ? "CORRETO!" : "INCORRETO!");
 
-        printf("\n%s\n", string);
-        printf("\n%s\n", string2);
 
-        return 0;
+	printf("==>> DESALOCANDO A PILHA (ILEGAL):\n");
+	unsigned int alloc_return = memory_free((void*) alloc_pointers);
+	if (!alloc_return) printf("\tO RETORNO DA LIBERAÇÃO FOI NULL!\n");
+
+	return 0;
 }
